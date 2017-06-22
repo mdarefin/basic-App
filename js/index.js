352811ff78d685da854ffc85ea2bf15e8b0bb294
Md Arefin <arefin.shahrear@gmail.com>
2017-06-22 17:23:23 -0400

    graphs

---
+++ js/index.js
@@ -0,0 +1,30 @@
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July", "August"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40, -30]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.5)",
+            strokeColor: "rgba(151,187,205,0.8)",
+            highlightFill: "rgba(151,187,205,0.75)",
+            highlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90, -42]
+        }
+    ]
+};
+
+var options = {
+    scaleBeginAtZero: false,
+    responsive: true,
+    scaleStartValue : -50 
+};
+
+var ctx = document.getElementById("myChart").getContext("2d");
+var myBarChart = new Chart(ctx).Bar(data, options);
