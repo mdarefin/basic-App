b53eb6f717c5ce5fdca729919e9696a773915ea7
Md Arefin <arefin.shahrear@gmail.com>
2017-06-22 17:30:33 -0400

    renaming

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July", "August"],
+    labels: ["9am", "11am", "1pm", "3pm", "5pm", "7pm", "9pm", "11pm"],
     datasets: [
         {
             label: "My First dataset",
@@ -15,7 +15,7 @@ var data = {
             strokeColor: "rgba(151,187,205,0.8)",
             highlightFill: "rgba(151,187,205,0.75)",
             highlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90, -42]
+            data: [28, 48, 40, 19, 86, 27, 90, -82]
         }
     ]
 };
